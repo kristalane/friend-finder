993a4971d10fde7987ceca7fdb032e69e94ff76a
Krista Lane <klane@Kristas-MacBook-Air.local>
2017-08-04 00:06:30 -0700

    fixed home.html routing issue and working on sending data from survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,14 +4,14 @@ var appDir = path.dirname(require.main.filename);
 
 module.exports = function(app) {
 
-  // serve the home page
-  app.use("/", function(req, res) {
-    res.sendFile(path.join(appDir, "app", "public", "home.html"));
-  });
-
   // serve the survey page
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(appDir, "app", "public", "survey.html"));
+  });
+
+  // if not specified, default to home
+  app.use(function(req, res) {
+    res.sendFile(path.join(appDir, "app", "public", "home.html"));
   });
 
 };
