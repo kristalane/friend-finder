03cac682ce446df9d6c0861b0746820db8352aca
Krista Lane <klane@Kristas-MacBook-Air.local>
2017-08-04 20:11:12 -0700

    updating footer links in html and removing typos in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 
 var friend1 = {
-  "name":"Tony",
-  "photo": "http://vignette1.wikia.nocookie.net/suburgatory/images/5/52/Happy_face.jpg/revision/latest?cb=20120412051836",
-  "scores": [
+  name:"Jake",
+  photo: "http://i677.photobucket.com/albums/vv134/shelbywrks/1425117474_7c496d6efa.jpg",
+  scores: [
     2,
     4,
     5,
@@ -16,6 +16,23 @@ var friend1 = {
   ]
 };
 
-var friends = [friend1];
+var friend2 = {
+  name: "Kyle",
+  photo: "https://assets.fastcompany.com/image/upload/w_596,c_limit,q_auto:best,f_auto,fl_lossy/fc/1681630-inline-inline-1-the-creation-of-a-classic-character-how-ron-swanson-became-ron-swanson.jpg",
+  scores: [
+    5,
+    5,
+    4,
+    5,
+    1,
+    2,
+    1,
+    3,
+    5,
+    2
+  ]
+};
+
+var friends = [friend1, friend2];
 
 module.exports = friends;
